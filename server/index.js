c1465d1bc8ca68a5a18cece6540c30c9e5d54b30
Harsh Wardhan <harshwardhan847@gmail.com>
2026-05-15 11:07:54 +0530

    basic setup done

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import { Queue } from 'bullmq';
-import { OpenAIEmbeddings } from '@langchain/openai';
-import { QdrantVectorStore } from '@langchain/qdrant';
-import OpenAI from 'openai';
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { Queue } from "bullmq";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import OpenAI from "openai";
+import "dotenv/config";
 
 const client = new OpenAI({
-  apiKey: '',
+  apiKey: process.env.OPENAI_API_KEY,
 });
-const queue = new Queue('file-upload-queue', {
+const queue = new Queue("file-upload-queue", {
   connection: {
-    host: 'localhost',
-    port: '6379',
+    host: "localhost",
+    port: "6379",
   },
 });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, `${uniqueSuffix}-${file.originalname}`);
   },
 });
@@ -31,35 +32,35 @@ const upload = multer({ storage: storage });
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.json({ status: 'All Good!' });
+app.get("/", (req, res) => {
+  return res.json({ status: "All Good!" });
 });
 
-app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
+app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
   await queue.add(
-    'file-ready',
+    "file-ready",
     JSON.stringify({
       filename: req.file.originalname,
       destination: req.file.destination,
       path: req.file.path,
-    })
+    }),
   );
-  return res.json({ message: 'uploaded' });
+  return res.json({ message: "uploaded" });
 });
 
-app.get('/chat', async (req, res) => {
+app.get("/chat", async (req, res) => {
   const userQuery = req.query.message;
 
   const embeddings = new OpenAIEmbeddings({
-    model: 'text-embedding-3-small',
-    apiKey: '',
+    model: "text-embedding-3-small",
+    apiKey: process.env.OPENAI_API_KEY,
   });
   const vectorStore = await QdrantVectorStore.fromExistingCollection(
     embeddings,
     {
-      url: 'http://localhost:6333',
-      collectionName: 'langchainjs-testing',
-    }
+      url: "http://localhost:6333",
+      collectionName: "langchainjs-testing",
+    },
   );
   const ret = vectorStore.asRetriever({
     k: 2,
@@ -73,10 +74,10 @@ app.get('/chat', async (req, res) => {
   `;
 
   const chatResult = await client.chat.completions.create({
-    model: 'gpt-4.1',
+    model: "gpt-4.1",
     messages: [
-      { role: 'system', content: SYSTEM_PROMPT },
-      { role: 'user', content: userQuery },
+      { role: "system", content: SYSTEM_PROMPT },
+      { role: "user", content: userQuery },
     ],
   });
 
--- server/worker.js
+++ server/worker.js
@@ -1,12 +1,12 @@
-import { Worker } from 'bullmq';
-import { OpenAIEmbeddings } from '@langchain/openai';
-import { QdrantVectorStore } from '@langchain/qdrant';
-import { Document } from '@langchain/core/documents';
-import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
-import { CharacterTextSplitter } from '@langchain/textsplitters';
+import { Worker } from "bullmq";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 
 const worker = new Worker(
-  'file-upload-queue',
+  "file-upload-queue",
   async (job) => {
     console.log(`Job:`, job.data);
     const data = JSON.parse(job.data);
@@ -23,16 +23,16 @@ const worker = new Worker(
     const docs = await loader.load();
 
     const embeddings = new OpenAIEmbeddings({
-      model: 'text-embedding-3-small',
-      apiKey: '',
+      model: "text-embedding-3-small",
+      apiKey: process.env.OPENAI_API_KEY,
     });
 
     const vectorStore = await QdrantVectorStore.fromExistingCollection(
       embeddings,
       {
-        url: 'http://localhost:6333',
-        collectionName: 'langchainjs-testing',
-      }
+        url: "http://localhost:6333",
+        collectionName: "langchainjs-testing",
+      },
     );
     await vectorStore.addDocuments(docs);
     console.log(`All docs are added to vector store`);
@@ -40,8 +40,8 @@ const worker = new Worker(
   {
     concurrency: 100,
     connection: {
-      host: 'localhost',
-      port: '6379',
+      host: "localhost",
+      port: "6379",
     },
-  }
+  },
 );
